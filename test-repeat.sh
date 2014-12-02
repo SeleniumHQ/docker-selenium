cab6461b3a1008a1aafd85fc53040d700bf058ff
Matthew Smith <mtscout6@gmail.com>
2014-12-02 15:18:19 -0700

    Add test-repeat script
    Simply re-runs the chrome smoke test 50 times in a row.

---
+++ test-repeat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+COUNTER=0
+
+while [ $COUNTER -lt 50 ]; do
+  echo The counter is $COUNTER
+  let COUNTER=COUNTER+1
+  docker run --rm -it --link $1:hub selenium/test:local node smoke-chrome.js
+done
