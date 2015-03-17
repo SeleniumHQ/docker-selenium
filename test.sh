562ce068edd05136fe54fc91c483d0c01ec0bcd3
Eric Edgar <eedgar@zenoss.com>
2015-03-17 09:55:37 -0500

    Add unit tests that test the htmlunit feature

--- test.sh
+++ test.sh
@@ -15,15 +15,21 @@ sleep 2
 
 NODE_CHROME=$(docker run -d --link $HUB_NAME:hub  selenium/node-chrome$DEBUG:2.45.0)
 NODE_FIREFOX=$(docker run -d --link $HUB_NAME:hub selenium/node-firefox$DEBUG:2.45.0)
+NODE_HTMLUNIT=$(docker run -d --link $HUB_NAME:hub selenium/node-htmlunit$DEBUG:2.45.0)
 
 docker logs -f $HUB &
 docker logs -f $NODE_CHROME &
 docker logs -f $NODE_FIREFOX &
+docker logs -f $NODE_HTMLUNIT &
 sleep 2
 
 echo Running test container...
 docker run --rm -it --link $HUB_NAME:hub selenium/test:local
 STATUS=$?
+
+echo Tearing down Selenium HtmlUnit Node container
+docker stop $NODE_HTMLUNIT
+docker rm $NODE_HTMLUNIT
 
 echo Tearing down Selenium Chrome Node container
 docker stop $NODE_CHROME
