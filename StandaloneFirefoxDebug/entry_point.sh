e36109e30fbdb5a448571583b32d2d57f3bcb00f
Mathieu POUSSE <m.mathieu.pousse@gmail.com>
2015-12-03 22:37:38 +0100

    manullay transmit env variables to selenium user
    Signed-off-by: Mathieu POUSSE <m.mathieu.pousse@gmail.com>

--- StandaloneFirefoxDebug/entry_point.sh
+++ StandaloneFirefoxDebug/entry_point.sh
@@ -10,7 +10,10 @@ if [ ! -z "$SE_OPTS" ]; then
   echo "appending selenium options: ${SE_OPTS}"
 fi
 
+env | cut -f 1 -d "=" | sort > asroot
+sudo -E -u seluser -i env | cut -f 1 -d "=" | sort > asseluser
 sudo -E -i -u seluser \
+  $(for E in $(grep -vxFf asseluser asroot); do echo $E=$(eval echo \$$E); done) \
   DISPLAY=$DISPLAY \
   xvfb-run --server-args="$DISPLAY -screen 0 $GEOMETRY -ac +extension RANDR" \
   java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
