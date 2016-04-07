c01b757588dbaaa4e7b5666bbc34dfd45941a303
David Maes <david.s.maes@gmail.com>
2016-04-07 13:49:45 +0200

    Added force flag to remove command

--- StandaloneChromeDebug/clear_x_locks.sh
+++ StandaloneChromeDebug/clear_x_locks.sh
@@ -5,6 +5,6 @@ file="/tmp/.X??-lock"
 echo "Looking for lock file: $file"
 if [ -f $file ] ; then
     echo "Lock file: $file FOUND"
-    rm $file
+    rm -f $file
     echo "Lock file: $file REMOVED"
 fi
