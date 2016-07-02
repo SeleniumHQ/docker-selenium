5d91a000e5c99d7abf31e96d45c34a94f3ed1ad7
Charles G <charles@chuckg.org>
2016-07-01 17:07:03 -0700

    Fix environment variables with spaces during xvfb boot

--- StandaloneDebug/entry_point.sh
+++ StandaloneDebug/entry_point.sh
@@ -20,12 +20,12 @@ rm -f /tmp/.X*lock
 SERVERNUM=$(get_server_num)
 env | cut -f 1 -d "=" | sort > asroot
 sudo -E -u seluser -i env | cut -f 1 -d "=" | sort > asseluser
-sudo -E -i -u seluser \
-  $(for E in $(grep -vxFf asseluser asroot); do echo $E=$(eval echo \$$E); done) \
+sudo -E -i -u seluser bash -c "\
+  $(for E in $(grep -vxFf asseluser asroot); do echo $E="'$(eval echo \$$E)'"; done) \
   DISPLAY=$DISPLAY \
-  xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
+  xvfb-run -n $SERVERNUM --server-args=\"-screen 0 $GEOMETRY -ac +extension RANDR\" \
   java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
-  ${SE_OPTS} &
+  ${SE_OPTS} &"
 NODE_PID=$!
 
 trap shutdown SIGTERM SIGINT
