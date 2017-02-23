ade724e3d72d786fbbd3dcfaca41386c17a3a989
Unknown <camiller@frontlineed.com>
2017-02-23 12:08:03 -0500

    Updated generate to take namespace as argument.

--- StandaloneDebug/generate.sh
+++ StandaloneDebug/generate.sh
@@ -3,12 +3,13 @@ FOLDER=../$1
 BASE=$2
 BROWSER=$3
 VERSION=$4
+NAMESPACE=$5
 
 echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" > $FOLDER/Dockerfile
 echo "# NOTE: DO *NOT* EDIT THIS FILE.  IT IS GENERATED." >> $FOLDER/Dockerfile
 echo "# PLEASE UPDATE Dockerfile.txt INSTEAD OF THIS FILE" >> $FOLDER/Dockerfile
 echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" >> $FOLDER/Dockerfile
-echo FROM selenium/$BASE:$VERSION >> $FOLDER/Dockerfile
+echo FROM $NAMESPACE/$BASE:$VERSION >> $FOLDER/Dockerfile
 cat ../NodeDebug/Dockerfile.txt >> $FOLDER/Dockerfile
 
 cp ./entry_point.sh $FOLDER
