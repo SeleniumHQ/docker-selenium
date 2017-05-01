b6ada5191f8f942ce30fdea7107d76c07cc66f0c
Diego Molina <diemol@gmail.com>
2017-05-01 21:50:38 +0200

    Using new paths and removing logic to export env vars from root to seluser, not needed anymore

--- NodeFirefoxDebug/entry_point.sh
+++ NodeFirefoxDebug/entry_point.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 source /opt/bin/functions.sh
-/opt/selenium/generate_config > /opt/selenium/config.json
+/opt/bin/generate_config > /home/seluser/selenium/config.json
 
 export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
 
-if [ ! -e /opt/selenium/config.json ]; then
+if [ ! -e /home/seluser/selenium/config.json ]; then
   echo No Selenium Node configuration file, the node-base image is not intended to be run directly. 1>&2
   exit 1
 fi
@@ -29,22 +29,16 @@ if [ ! -z "$SE_OPTS" ]; then
   echo "appending selenium options: ${SE_OPTS}"
 fi
 
-# TODO: Look into http://www.seleniumhq.org/docs/05_selenium_rc.jsp#browser-side-logs
-
 SERVERNUM=$(get_server_num)
 
 rm -f /tmp/.X*lock
 
-env | cut -f 1 -d "=" | sort > asroot
-  sudo -E -u seluser -i env | cut -f 1 -d "=" | sort > asseluser
-  sudo -E -i -u seluser \
-  "$(for E in $(grep -vxFf asseluser asroot); do echo $E=$(eval echo \$$E); done)" \
-  DISPLAY=$DISPLAY \
+DISPLAY=$DISPLAY \
   xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
-  java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+  java ${JAVA_OPTS} -jar /home/seluser/selenium/selenium-server-standalone.jar \
     -role node \
     -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
-    -nodeConfig /opt/selenium/config.json \
+    -nodeConfig /home/seluser/selenium/config.json \
     ${SE_OPTS} &
 NODE_PID=$!
 
