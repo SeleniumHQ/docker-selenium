fa17e0393f89cf84282a7cd5a56d0a91f1ff0696
jsa34 <31512041+jsa34@users.noreply.github.com>
2018-06-30 16:18:39 +0100

    Update to python bindings to 3.13.0
    Matching global selenium update version

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd tests
-pip install selenium===3.11.0 \
+pip install selenium===3.13.0 \
             docker===3.1.1 \
             | grep -v 'Requirement already satisfied'
 
