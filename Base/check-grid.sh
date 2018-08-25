8278ba0ee6093d58c528716f0f2a9e4d63a68c8f
Diego Molina <diemol@gmail.com>
2018-08-25 11:37:58 +0200

    Adding default vars for script

--- Base/check-grid.sh
+++ Base/check-grid.sh
@@ -3,18 +3,19 @@
 
 set -e
 
+HOST="localhost"
+PORT="4444"
+
 # process arguments
 while [[ $# -gt 0 ]]
 do
     case "$1" in
         --host)
-        HOST="$2"
-        if [[ ${HOST} == "" ]]; then HOST="localhost"; fi
+        HOST=${2:-"localhost"}
         shift 2
         ;;
         --port)
-        PORT="$2"
-        if [[ ${PORT} == "" ]]; then PORT="4444"; fi
+        PORT=${2:-"4444"}
         shift 2
         ;;
         *)
