7e207fb4cadbde578395c1821d9b0c7cde2e3b7e
Antonello Caboni <antonello.caboni@wahanda.com>
2018-09-13 14:35:30 +0100

    Start from a random number when xvfb looks for available display (#785)

--- Standalone/entry_point.sh
+++ Standalone/entry_point.sh
@@ -2,6 +2,8 @@
 #
 # IMPORTANT: Change this file only in directory Standalone!
 
+source /opt/bin/functions.sh
+
 export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
 
 function shutdown {
@@ -13,9 +15,11 @@ if [ ! -z "$SE_OPTS" ]; then
   echo "appending selenium options: ${SE_OPTS}"
 fi
 
+SERVERNUM=$(get_random_server_number)
+
 rm -f /tmp/.X*lock
 
-xvfb-run -a --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
+xvfb-run -a -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
   java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
   ${SE_OPTS} &
 NODE_PID=$!
--- StandaloneDebug/entry_point.sh
+++ StandaloneDebug/entry_point.sh
@@ -2,6 +2,8 @@
 #
 # IMPORTANT: Change this file only in directory StandaloneDebug!
 
+source /opt/bin/functions.sh
+
 export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
 
 function shutdown {
@@ -20,11 +22,13 @@ if [ ! -z "$SE_OPTS" ]; then
   echo "appending selenium options: ${SE_OPTS}"
 fi
 
+SERVERNUM=$(get_random_server_number)
+
 rm -f /tmp/.X*lock
 
 # Creating a file descriptor, where the DISPLAY will be saved ("6" was arbitrarily chosen)
 exec 6>/tmp/display.log
-xvfb-run -a --server-args="-screen 0 $GEOMETRY -ac +extension RANDR -displayfd 6" \
+xvfb-run -a -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR -displayfd 6" \
   java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
   ${SE_OPTS} &
 NODE_PID=$!
