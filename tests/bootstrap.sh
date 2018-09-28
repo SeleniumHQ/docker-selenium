1b75056483fe660a158d1f3d398043d75d6b4a44
Diego Molina <diemol@users.noreply.github.com>
2018-09-28 23:18:30 +0200

    Upgrading docker bindings for testing to ver. 3.5.0 (#792)

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd tests
 pip install selenium===3.14.0 \
-            docker===3.1.1 \
+            docker===3.5.0 \
             | grep -v 'Requirement already satisfied'
 
 python test.py $1 $2
