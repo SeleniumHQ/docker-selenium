3a9c4859bfeefeb9f3671f84afd6c63acbf26b0a
Diego Molina <diemol@gmail.com>
2018-10-12 13:44:12 +0200

    Adjusting container permissions for OpenShift and other non-sudo environments. Fixes #802

--- Base/entry_point.sh
+++ Base/entry_point.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+#==============================================
+# OpenShift or non-sudo environments support
+# https://docs.openshift.com/container-platform/3.11/creating_images/guidelines.html#openshift-specific-guidelines
+#==============================================
+
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
+
 /usr/bin/supervisord --configuration /etc/supervisord.conf &
 
 SUPERVISOR_PID=$!
