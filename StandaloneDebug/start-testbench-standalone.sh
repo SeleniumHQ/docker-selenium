bcb7064738a2c92bc4945ae91aed93b01570b283
business <business@172.16.50.202>
2019-01-17 13:24:59 +0100

    Migrate standalone-debug template to Vaadin Testbench

--- StandaloneDebug/start-testbench-standalone.sh
+++ StandaloneDebug/start-testbench-standalone.sh
@@ -2,5 +2,5 @@
 #
 # IMPORTANT: Change this file only in directory StandaloneDebug!
 
-java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+java ${JAVA_OPTS} -jar /opt/selenium/vaadin-testbench-standalone.jar \
     ${SE_OPTS}
