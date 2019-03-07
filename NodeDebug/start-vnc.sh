0df726086e79fd79dc47e209da37a06d767cb88f
Diego Molina <diemol@gmail.com>
2019-03-07 14:25:54 +0100

    Fix crashes in x11vnc, credit to @philippmoeller

--- NodeDebug/start-vnc.sh
+++ NodeDebug/start-vnc.sh
@@ -20,7 +20,8 @@ if [ "${START_XVFB}" = true ] ; then
     echo "Waiting for Xvfb..."
   done
 
-  x11vnc ${X11VNC_OPTS} -forever -shared -rfbport 5900 -display ${DISPLAY}
+  # -noxrecord fixes https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=859213 in x11vnc 0.9.13-2
+  x11vnc ${X11VNC_OPTS} -forever -shared -rfbport 5900 -display ${DISPLAY} -noxrecord
 else
   echo "Vnc won't start because Xvfb is configured to not start."
 fi
