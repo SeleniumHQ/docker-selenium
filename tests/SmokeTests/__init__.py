f46abe8f6f8462df939cd3fda101073318516fcf
Diego Molina <diemol@gmail.com>
2020-10-02 18:16:56 +0200

    Making the Grid host an env var

--- tests/SmokeTests/__init__.py
+++ tests/SmokeTests/__init__.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import time
 import json
@@ -6,6 +7,8 @@ try:
     from urllib2 import urlopen
 except ImportError:
     from urllib.request import urlopen
+
+SELENIUM_GRID_HOST = os.environ.get('SELENIUM_GRID_HOST', 'localhost')
 
 
 class SmokeTests(unittest.TestCase):
@@ -19,7 +22,7 @@ class SmokeTests(unittest.TestCase):
         while current_attempts < max_attempts:
             current_attempts = current_attempts + 1
             try:
-                response = urlopen('http://localhost:%s/status' % port)
+                response = urlopen('http://%s:%s/status' % (SELENIUM_GRID_HOST, port))
                 status_json = json.loads(response.read())
                 self.assertTrue(status_json['value']['ready'], "Container is not ready on port %s" % port)
                 status_fetched = True
