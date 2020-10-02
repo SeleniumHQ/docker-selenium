26c927a16e393a821cd25c4be075a90a0a5f6b66
Diego Molina <diemol@gmail.com>
2020-10-03 00:50:19 +0200

    Using env var for GitHub actions

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-cd tests
+cd tests || true
 
-if [ "${TRAVIS:-false}" = "false" ]; then
+if [ "${CI:-false}" = "false" ]; then
   pip install virtualenv | grep -v 'Requirement already satisfied'
   virtualenv docker-selenium-tests
   source docker-selenium-tests/bin/activate
 fi
-
 
 python -m pip install selenium===3.14.1 \
                       docker===4.2.0 \
@@ -15,7 +14,7 @@ python -m pip install selenium===3.14.1 \
 python test.py $1
 ret_code=$?
 
-if [ "${TRAVIS:-false}" = "false" ]; then
+if [ "${CI:-false}" = "false" ]; then
   deactivate
 fi
 
