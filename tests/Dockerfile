a09631c65e817c0a1e8d1783d0faef9b814cea26
Diego Molina <diemol@users.noreply.github.com>
2020-10-03 02:43:43 +0200

    Adding a test to verify the video file is generated (#1115)

---
+++ tests/Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.8.6-buster
+
+WORKDIR /usr/src/app
+
+COPY . .
--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
-cd tests
+cd tests || true
 
-if [ "${TRAVIS:-false}" = "false" ]; then
+if [ "${CI:-false}" = "false" ]; then
   pip install virtualenv | grep -v 'Requirement already satisfied'
   virtualenv docker-selenium-tests
   source docker-selenium-tests/bin/activate
 fi
 
-
 python -m pip install selenium===3.14.1 \
                       docker===4.2.0 \
                       | grep -v 'Requirement already satisfied'
 
-python test.py $1 $2
+python test.py $1
 ret_code=$?
 
-if [ "${TRAVIS:-false}" = "false" ]; then
+if [ "${CI:-false}" = "false" ]; then
   deactivate
 fi
 
