6cbeb505d46d7451d5b463167339c1e79c99b24a
Diego Molina <diemol@gmail.com>
2020-11-18 13:26:45 +0100

    Mapping env vars for Hub

--- Hub/start-selenium-grid-hub.sh
+++ Hub/start-selenium-grid-hub.sh
@@ -3,10 +3,17 @@
 # set -e: exit asap if a command exits with a non-zero status
 set -e
 
-echo "Starting Selenium Grid Hub..."
+CONFIG_TOML_FILE=/opt/selenium/config.toml
+
+/opt/bin/generate_config >${CONFIG_TOML_FILE}
+
+echo "Starting Selenium Grid Hub with configuration: "
+cat ${CONFIG_TOML_FILE}
 
 if [ ! -z "$SE_OPTS" ]; then
   echo "Appending Selenium options: ${SE_OPTS}"
 fi
 
-java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar hub --relax-checks true ${SE_OPTS}
+java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar hub \
+  --config ${CONFIG_TOML_FILE} \
+  ${SE_OPTS}
