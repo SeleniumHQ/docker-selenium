c7924e78fdc888337e00e03bddbddc47c0918dfd
Diego Molina <diemol@users.noreply.github.com>
2020-11-18 09:29:00 +0100

    Adding session queuer (#1138)

--- Router/start-selenium-grid-router.sh
+++ Router/start-selenium-grid-router.sh
@@ -25,11 +25,22 @@ if [[ -z "${SE_DISTRIBUTOR_PORT}" ]]; then
   exit 1
 fi
 
+if [[ -z "${SE_SESSION_QUEUER_HOST}" ]]; then
+  echo "SE_SESSION_QUEUER_HOST not set, exiting!" 1>&2
+  exit 1
+fi
+
+if [[ -z "${SE_SESSION_QUEUER_PORT}" ]]; then
+  echo "SE_SESSION_QUEUER_PORT not set, exiting!" 1>&2
+  exit 1
+fi
+
 if [ ! -z "$SE_OPTS" ]; then
   echo "Appending Selenium options: ${SE_OPTS}"
 fi
 
 java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar router \
-  --sessions-host "${SE_SESSIONS_MAP_HOST}" --sessions-port ${SE_SESSIONS_MAP_PORT} \
-  --distributor-host "${SE_DISTRIBUTOR_HOST}" --distributor-port ${SE_DISTRIBUTOR_PORT} \
+  --sessions-host "${SE_SESSIONS_MAP_HOST}" --sessions-port "${SE_SESSIONS_MAP_PORT}" \
+  --distributor-host "${SE_DISTRIBUTOR_HOST}" --distributor-port "${SE_DISTRIBUTOR_PORT}" \
+  --sessionqueuer-host "${SE_SESSION_QUEUER_HOST}" --sessionqueuer-port "${SE_SESSION_QUEUER_PORT}" \
   --relax-checks true ${SE_OPTS}
---
+++ SessionQueuer/start-selenium-grid-session-queuer.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# set -e: exit asap if a command exits with a non-zero status
+set -e
+
+echo "Starting Selenium Grid SessionQueuer..."
+
+if [[ -z "${SE_EVENT_BUS_HOST}" ]]; then
+  echo "SE_EVENT_BUS_HOST not set, exiting!" 1>&2
+  exit 1
+fi
+
+if [[ -z "${SE_EVENT_BUS_PUBLISH_PORT}" ]]; then
+  echo "SE_EVENT_BUS_PUBLISH_PORT not set, exiting!" 1>&2
+  exit 1
+fi
+
+if [[ -z "${SE_EVENT_BUS_SUBSCRIBE_PORT}" ]]; then
+  echo "SE_EVENT_BUS_SUBSCRIBE_PORT not set, exiting!" 1>&2
+  exit 1
+fi
+
+if [ ! -z "$SE_OPTS" ]; then
+  echo "Appending Selenium options: ${SE_OPTS}"
+fi
+
+java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar sessionqueuer \
+  --publish-events tcp://"${SE_EVENT_BUS_HOST}":${SE_EVENT_BUS_PUBLISH_PORT} \
+  --subscribe-events tcp://"${SE_EVENT_BUS_HOST}":${SE_EVENT_BUS_SUBSCRIBE_PORT} \
+  ${SE_OPTS}
