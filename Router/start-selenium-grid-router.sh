46fd8fec2d680209751a2062cd17121bdd918bfe
Diego Molina <diemol@gmail.com>
2020-11-30 16:59:39 +0100

    Enabling host and port config for the Router

--- Router/start-selenium-grid-router.sh
+++ Router/start-selenium-grid-router.sh
@@ -39,8 +39,21 @@ if [ ! -z "$SE_OPTS" ]; then
   echo "Appending Selenium options: ${SE_OPTS}"
 fi
 
+if [ ! -z "$SE_ROUTER_HOST" ]; then
+  echo "Using SE_ROUTER_HOST: ${SE_ROUTER_HOST}"
+  HOST_CONFIG="--host ${SE_ROUTER_HOST}"
+fi
+
+if [ ! -z "$SE_ROUTER_PORT" ]; then
+  echo "Using SE_ROUTER_PORT: ${SE_ROUTER_PORT}"
+  PORT_CONFIG="--port ${SE_ROUTER_PORT}"
+fi
+
 java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar router \
   --sessions-host "${SE_SESSIONS_MAP_HOST}" --sessions-port "${SE_SESSIONS_MAP_PORT}" \
   --distributor-host "${SE_DISTRIBUTOR_HOST}" --distributor-port "${SE_DISTRIBUTOR_PORT}" \
   --sessionqueuer-host "${SE_SESSION_QUEUER_HOST}" --sessionqueuer-port "${SE_SESSION_QUEUER_PORT}" \
-  --relax-checks true ${SE_OPTS}
+  --relax-checks true \
+  ${HOST_CONFIG} \
+  ${PORT_CONFIG} \
+  ${SE_OPTS}
