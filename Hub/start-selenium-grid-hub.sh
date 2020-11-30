e55228b0b2e188c1a58dfd8a9087f337aa8905c0
Diego Molina <diemol@gmail.com>
2020-11-30 17:07:50 +0100

    Fixing Hub startup

--- Hub/start-selenium-grid-hub.sh
+++ Hub/start-selenium-grid-hub.sh
@@ -3,13 +3,6 @@
 # set -e: exit asap if a command exits with a non-zero status
 set -e
 
-CONFIG_TOML_FILE=/opt/selenium/config.toml
-
-/opt/bin/generate_config >${CONFIG_TOML_FILE}
-
-echo "Starting Selenium Grid Hub with configuration: "
-cat ${CONFIG_TOML_FILE}
-
 if [ ! -z "$SE_OPTS" ]; then
   echo "Appending Selenium options: ${SE_OPTS}"
 fi
