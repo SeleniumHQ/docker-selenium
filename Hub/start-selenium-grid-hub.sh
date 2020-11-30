5a98568bffd2cb5d6061f8db112f4a1269e9abd3
Diego Molina <diemol@gmail.com>
2020-11-30 16:54:11 +0100

    Enabling host and port config for the Hub

--- Hub/start-selenium-grid-hub.sh
+++ Hub/start-selenium-grid-hub.sh
@@ -14,6 +14,22 @@ if [ ! -z "$SE_OPTS" ]; then
   echo "Appending Selenium options: ${SE_OPTS}"
 fi
 
+if [ ! -z "$SE_HUB_HOST" ]; then
+  echo "Using SE_HUB_HOST: ${SE_HUB_HOST}"
+  HOST_CONFIG="--host ${SE_HUB_HOST}"
+fi
+
+if [ ! -z "$SE_HUB_PORT" ]; then
+  echo "Using SE_HUB_PORT: ${SE_HUB_PORT}"
+  PORT_CONFIG="--port ${SE_HUB_PORT}"
+fi
+
+
 java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar hub \
-  --config ${CONFIG_TOML_FILE} \
+  --session-request-timeout ${SE_SESSION_REQUEST_TIMEOUT} \
+  --session-retry-interval ${SE_SESSION_RETRY_INTERVAL} \
+  --allow-cors ${SE_ALLOW_CORS} \
+  --relax-checks ${SE_RELAX_CHECKS} \
+  ${HOST_CONFIG} \
+  ${PORT_CONFIG} \
   ${SE_OPTS}
