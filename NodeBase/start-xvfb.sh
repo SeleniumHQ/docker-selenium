38071cbfc39af66571b57496576fcca120844f00
qxo <49526356@qq.com>
2020-12-20 10:39:42 +0800

    feat: xvfb add `-fbdir /var/tmp` for screen record by selenium-video-node
    ```
    -Dvideo.xvfbscreen=/var/tmp
    ```
    ref: https://github.com/aimmac23/selenium-video-node/blob/master/XVFB_ACCELERATION.md

--- NodeBase/start-xvfb.sh
+++ NodeBase/start-xvfb.sh
@@ -11,7 +11,7 @@ if [ "${START_XVFB}" = true ] ; then
   # http://manpages.ubuntu.com/manpages/bionic/man1/Xserver.1.html
   /usr/bin/xvfb-run --server-num=${DISPLAY_NUM} \
     --listen-tcp \
-    --server-args="-screen 0 ${GEOMETRY} -dpi ${SCREEN_DPI} -listen tcp -noreset -ac +extension RANDR" \
+    --server-args="-screen 0 ${GEOMETRY} -fbdir /var/tmp -dpi ${SCREEN_DPI} -listen tcp -noreset -ac +extension RANDR" \
     /usr/bin/fluxbox -display ${DISPLAY}
 else
   echo "Xvfb and Fluxbox won't start. Chrome/Firefox/Opera can only run in headless mode. Remember to set the 'headless' flag in your test."
