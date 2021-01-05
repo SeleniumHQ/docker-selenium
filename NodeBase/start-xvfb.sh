1780c1c5eeb34629758cac37fabaa02ee771c6e4
Hakim <4447631+hbouhadji@users.noreply.github.com>
2021-01-05 15:41:49 +0000

    Update to Ubuntu 20.04 (#1169)
    * Update to Ubuntu 20.04
    
    * Fix libffmpeg error on ubuntu 20.04 (opera)
    
    Co-authored-by: Diego Molina <diemol@users.noreply.github.com>

--- NodeBase/start-xvfb.sh
+++ NodeBase/start-xvfb.sh
@@ -6,9 +6,9 @@ if [ "${START_XVFB}" = true ] ; then
   rm -f /tmp/.X*lock
 
   # Command reference
-  # http://manpages.ubuntu.com/manpages/bionic/man1/xvfb-run.1.html
-  # http://manpages.ubuntu.com/manpages/bionic/man1/Xvfb.1.html
-  # http://manpages.ubuntu.com/manpages/bionic/man1/Xserver.1.html
+  # http://manpages.ubuntu.com/manpages/focal/man1/xvfb-run.1.html
+  # http://manpages.ubuntu.com/manpages/focal/man1/Xvfb.1.html
+  # http://manpages.ubuntu.com/manpages/focal/man1/Xserver.1.html
   /usr/bin/xvfb-run --server-num=${DISPLAY_NUM} \
     --listen-tcp \
     --server-args="-screen 0 ${GEOMETRY} -fbdir /var/tmp -dpi ${SCREEN_DPI} -listen tcp -noreset -ac +extension RANDR" \
--- NodeOpera/generate.sh
+++ NodeOpera/generate.sh
@@ -7,6 +7,9 @@ echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" > ./Dockerfile
 echo "# NOTE: DO *NOT* EDIT THIS FILE.  IT IS GENERATED." >> ./Dockerfile
 echo "# PLEASE UPDATE Dockerfile.txt INSTEAD OF THIS FILE" >> ./Dockerfile
 echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" >> ./Dockerfile
+echo "FROM ubuntu:bionic AS builder-codecs-ffmpeg" >> ./Dockerfile
+echo "RUN apt-get update -qqy && apt-get -qqy install chromium-codecs-ffmpeg-extra" >> ./Dockerfile
+echo "" >> ./Dockerfile
 echo FROM ${NAMESPACE}/node-base:${VERSION} >> ./Dockerfile
 echo LABEL authors="$AUTHORS" >> ./Dockerfile
 echo "" >> ./Dockerfile
