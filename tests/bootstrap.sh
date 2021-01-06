d05ffa0a9b5143c7456ca904d648a080beb876c4
Diego Molina <diemol@users.noreply.github.com>
2021-01-06 20:44:20 +0100

    Grid 4 Beta 1 (2e2ec56b20) - Prerelease [deploy][prerelease]

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -7,7 +7,7 @@ if [ "${CI:-false}" = "false" ]; then
   source docker-selenium-tests/bin/activate
 fi
 
-python -m pip install selenium===3.14.1 \
+python -m pip install selenium==4.0.0a6.post2 \
                       docker===4.2.0 \
                       | grep -v 'Requirement already satisfied'
 
