bbd535f8f0f054fe817520d4e6714311e4885d93
Diego Molina <diemol@gmail.com>
2021-05-30 21:40:11 +0200

    Adding endpoint to check video recording started

---
+++ Video/video_ready.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler,HTTPServer
+from os import environ
+import json
+import psutil
+
+video_ready_port = environ.get('VIDEO_READY_PORT', 9000)
+
+class Handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        video_ready = "ffmpeg" in (p.name().lower() for p in psutil.process_iter())
+        response_code = 200 if video_ready else 404
+        response_text = "ready" if video_ready else "not ready"
+        self.send_response(response_code)
+        self.end_headers()
+        self.wfile.write(json.dumps({'status': response_text}).encode('utf-8'))
+
+httpd = HTTPServer( ('0.0.0.0', video_ready_port), Handler )
+httpd.serve_forever()
