3cd5ea328428d4950bd65d561eb608e775ab978d
Jason Berenbach <jasonberenbach@gmail.com>
2021-09-08 12:12:51 -0400

    Added configurable env variables to support disabling noVnc and changing the ports

--- NodeBase/start-novnc.sh
+++ NodeBase/start-novnc.sh
@@ -3,7 +3,11 @@
 # IMPORTANT: Change this file only in directory NodeBase!
 
 if [ "${START_XVFB}" = true ] ; then
-  /opt/bin/noVNC/utils/launch.sh --listen 7900 --vnc localhost:5900
+  if [ "${START_NO_VNC}" = true ] ; then
+    /opt/bin/noVNC/utils/launch.sh --listen ${NO_VNC_PORT:-7900} --vnc localhost:${VNC_PORT:-5900}
+  else
+    echo "noVNC won't start because START_NO_VNC is false."
+  fi
 else
   echo "noVNC won't start because Xvfb is configured to not start."
 fi
