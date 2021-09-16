dc436329e8333f09e2a766b80d6ae4f6b5709a72
Diego Molina <diemol@gmail.com>
2021-09-16 12:40:37 +0200

    Appending Grid url to different roles

--- StandaloneDocker/start-selenium-grid-standalone-docker.sh
+++ StandaloneDocker/start-selenium-grid-standalone-docker.sh
@@ -9,8 +9,13 @@ if [ ! -z "$SE_OPTS" ]; then
   echo "Appending Selenium options: ${SE_OPTS}"
 fi
 
+if [ ! -z "$SE_NODE_GRID_URL" ]; then
+  echo "Appending Grid url: ${SE_NODE_GRID_URL}"
+  SE_GRID_URL="--grid-url ${SE_NODE_GRID_URL}"
+fi
+
 java ${JAVA_OPTS} -jar /opt/selenium/selenium-server.jar standalone \
   --relax-checks ${SE_RELAX_CHECKS} \
   --detect-drivers false \
   --config /opt/bin/config.toml \
-  ${SE_OPTS}
+  ${SE_GRID_URL} ${SE_OPTS}
