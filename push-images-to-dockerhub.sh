00eefd64d2641bacbace4a3d1c21cdfcd37bd3eb
James <jmort253@gmail.com>
2021-11-11 14:32:37 +0530

    Added helper scripts for pushing to dockerhub

---
+++ push-images-to-dockerhub.sh
@@ -0,0 +1,21 @@
+BUILD_DATE=$(date +'%Y%m%d')
+VERSION=4.0.0
+NAMESPACE=seleniarm
+
+
+# docker push seleniarm/base:4.0.0-20211111
+# docker push seleniarm/hub:4.0.0-20211111
+# docker push seleniarm/node-base:4.0.0-20211111
+# docker push seleniarm/node-chromium:4.0.0-20211111
+# docker push seleniarm/node-firefox:4.0.0-20211111
+# docker push seleniarm/standalone-chromium:4.0.0-20211111
+# docker push seleniarm/standalone-firefox:4.0.0-20211111
+
+docker push seleniarm/base:latest
+docker push seleniarm/hub:latest
+docker push seleniarm/node-base:latest
+docker push seleniarm/node-chromium:latest
+docker push seleniarm/node-firefox:latest
+docker push seleniarm/standalone-chromium:latest
+docker push seleniarm/standalone-firefox:latest
+
