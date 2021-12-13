6629e6f2e734385cfbd8d618c01f231a5e97e608
James <jmort253@gmail.com>
2021-12-13 12:06:58 +0530

    Fixed Issue 1 where google-chrome binary is not found. Instead, we use the chromium binary to get the browser version.

--- push-images-to-dockerhub.sh
+++ push-images-to-dockerhub.sh
@@ -3,13 +3,13 @@ VERSION=4.0.0
 NAMESPACE=seleniarm
 
 
-# docker push seleniarm/base:4.0.0-20211111
-# docker push seleniarm/hub:4.0.0-20211111
-# docker push seleniarm/node-base:4.0.0-20211111
-# docker push seleniarm/node-chromium:4.0.0-20211111
-# docker push seleniarm/node-firefox:4.0.0-20211111
-# docker push seleniarm/standalone-chromium:4.0.0-20211111
-# docker push seleniarm/standalone-firefox:4.0.0-20211111
+docker push seleniarm/base:4.0.0-$BUILD_DATE
+docker push seleniarm/hub:4.0.0-$BUILD_DATE
+docker push seleniarm/node-base:4.0.0-$BUILD_DATE
+docker push seleniarm/node-chromium:4.0.0-$BUILD_DATE
+docker push seleniarm/node-firefox:4.0.0-$BUILD_DATE
+docker push seleniarm/standalone-chromium:4.0.0-$BUILD_DATE
+docker push seleniarm/standalone-firefox:4.0.0-$BUILD_DATE
 
 docker push seleniarm/base:latest
 docker push seleniarm/hub:latest
