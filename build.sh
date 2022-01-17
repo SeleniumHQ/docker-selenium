8d57a8919718b03088d2d6a49da4ac8c91593b55
Colin <Coliinnn@users.noreply.github.com>
2022-01-17 23:22:43 +0100

    Support for 32-bit arm v7

--- build.sh
+++ build.sh
@@ -4,20 +4,20 @@ CHROMIUM=93.0.4577.82  # Not yet used at this time. Edit in NodeChromium/Dockerf
 NAMESPACE=local-seleniarm
 AUTHORS=SeleniumHQ,sj26,jamesmortensen
 
-cd ./Base && docker buildx build --platform linux/arm64 -t $NAMESPACE/base:$VERSION-$BUILD_DATE .
+cd ./Base && docker buildx build --platform linux/arm/v7 -t $NAMESPACE/base:$VERSION-$BUILD_DATE .
 echo $PWD
 cd ../Hub && sh generate.sh $VERSION-$BUILD_DATE $NAMESPACE $AUTHORS \
-   && docker buildx build --platform linux/arm64 -t $NAMESPACE/hub:$VERSION-$BUILD_DATE .
+   && docker buildx build --platform linux/arm/v7 -t $NAMESPACE/hub:$VERSION-$BUILD_DATE .
 
 cd ../NodeBase && sh generate.sh $VERSION-$BUILD_DATE $NAMESPACE $AUTHORS \
-   && docker buildx build --platform linux/arm64 -t $NAMESPACE/node-base:$VERSION-$BUILD_DATE .
+   && docker buildx build --platform linux/arm/v7 -t $NAMESPACE/node-base:$VERSION-$BUILD_DATE .
 # && sed 's/chromium=.*/chromium=91.0.4472.124/' Dockerfile > Dockerfile \
 cd ../NodeChromium && sh generate.sh $VERSION-$BUILD_DATE $NAMESPACE $AUTHORS \
-   && docker buildx build --platform linux/arm64 -t $NAMESPACE/node-chromium:$VERSION-$BUILD_DATE .
+   && docker buildx build --platform linux/arm/v7 -t $NAMESPACE/node-chromium:$VERSION-$BUILD_DATE .
 
 cd ../Standalone && sh generate.sh StandaloneChromium node-chromium $VERSION-$BUILD_DATE $NAMESPACE $AUTHORS \
    && cd ../StandaloneChromium \
-   && docker buildx build --platform linux/arm64 -t $NAMESPACE/standalone-chromium:$VERSION-$BUILD_DATE .
+   && docker buildx build --platform linux/arm/v7 -t $NAMESPACE/standalone-chromium:$VERSION-$BUILD_DATE .
 
 echo "Build node-hub, node-chromium, and standalone-chromium...\n"
 echo "Tagging builds...\n"
