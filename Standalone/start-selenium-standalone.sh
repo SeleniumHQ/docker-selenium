b31e016bf3886dada2d6ead6db031653f3b4b17b
Krishnan Mahadevan <krishnan.mahadevan1978@gmail.com>
2022-11-01 15:19:05 +0530

    Enable Tracing in Selenium Grid (#1711)
    Closes #1561

--- Standalone/start-selenium-standalone.sh
+++ Standalone/start-selenium-standalone.sh
@@ -12,9 +12,20 @@ echo "Selenium Grid Standalone configuration: "
 cat /opt/selenium/config.toml
 echo "Starting Selenium Grid Standalone..."
 
+EXTRA_LIBS="/opt/selenium/selenium-http-jdk-client.jar"
+
+if [ ! -z "$SE_ENABLE_TRACING" ]; then
+  EXTERNAL_JARS=$(</external_jars/.classpath.txt)
+  EXTRA_LIBS=${EXTRA_LIBS}:${EXTERNAL_JARS}
+  echo "Tracing is enabled"
+  echo "Classpath will be enriched with these external jars : " ${EXTRA_LIBS}
+else
+  echo "Tracing is disabled"
+fi
+
 java ${JAVA_OPTS:-$SE_JAVA_OPTS} -Dwebdriver.http.factory=jdk-http-client \
   -jar /opt/selenium/selenium-server.jar \
-  --ext /opt/selenium/selenium-http-jdk-client.jar standalone \
+  --ext ${EXTRA_LIBS} standalone \
   --bind-host ${SE_BIND_HOST} \
   --config /opt/selenium/config.toml \
   ${SE_OPTS}
