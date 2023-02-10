f4493df4acee2b8214c74ed29e4240ca1f87a4e5
budtmo <budtmo.os@gmail.com>
2023-02-10 14:43:06 +0100

    Using release tag instead of commit-id on novnc and websockify and updated both to the latest version

--- NodeBase/start-novnc.sh
+++ NodeBase/start-novnc.sh
@@ -4,7 +4,7 @@
 
 if [ "${START_XVFB:-$SE_START_XVFB}" = true ] ; then
   if [ "${START_NO_VNC:-$SE_START_NO_VNC}" = true ] ; then
-    /opt/bin/noVNC/utils/launch.sh --listen ${NO_VNC_PORT:-$SE_NO_VNC_PORT} --vnc localhost:${VNC_PORT:-$SE_VNC_PORT}
+    /opt/bin/noVNC/utils/novnc_proxy --listen ${NO_VNC_PORT:-$SE_NO_VNC_PORT} --vnc localhost:${VNC_PORT:-$SE_VNC_PORT}
   else
     echo "noVNC won't start because SE_START_NO_VNC is false."
   fi
