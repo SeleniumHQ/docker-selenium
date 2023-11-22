ad0a7a6fba5a5f123897d37c3e37e3bd45082b00
Diego Molina <diemol@users.noreply.github.com>
2023-11-22 16:31:31 +0100

    Setting driver and browser path via configuration (#2017)
    [deploy]

--- SessionQueue/start-selenium-grid-session-queue.sh
+++ SessionQueue/start-selenium-grid-session-queue.sh
@@ -19,10 +19,11 @@ if [ ! -z "$SE_SESSION_QUEUE_PORT" ]; then
   PORT_CONFIG="--port ${SE_SESSION_QUEUE_PORT}"
 fi
 
-EXTRA_LIBS="/opt/selenium/selenium-http-jdk-client.jar"
+EXTRA_LIBS=""
 
 if [ ! -z "$SE_ENABLE_TRACING" ]; then
   EXTERNAL_JARS=$(</external_jars/.classpath.txt)
+  EXTRA_LIBS="--ext "
   EXTRA_LIBS=${EXTRA_LIBS}:${EXTERNAL_JARS}
   echo "Tracing is enabled"
   echo "Classpath will be enriched with these external jars : " ${EXTRA_LIBS}
@@ -30,9 +31,9 @@ else
   echo "Tracing is disabled"
 fi
 
-java ${JAVA_OPTS:-$SE_JAVA_OPTS} -Dwebdriver.http.factory=jdk-http-client \
+java ${JAVA_OPTS:-$SE_JAVA_OPTS} \
   -jar /opt/selenium/selenium-server.jar \
-  --ext ${EXTRA_LIBS} sessionqueue \
+  ${EXTRA_LIBS} sessionqueue \
   --session-request-timeout ${SE_SESSION_REQUEST_TIMEOUT} \
   --session-retry-interval ${SE_SESSION_RETRY_INTERVAL} \
   --bind-host ${SE_BIND_HOST} \
