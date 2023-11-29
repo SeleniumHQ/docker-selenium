6b6840159472021fe037f4e2c74f9d5765f7d2c1
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2023-11-29 17:48:40 +0530

    bug(#2032): Rollback to use regular version in NodeChrome
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- generate_release_notes.sh
+++ generate_release_notes.sh
@@ -11,7 +11,7 @@ echo "" >> release_notes.md
 echo "### Changelog" > release_notes.md
 git --no-pager log "${LATEST_TAG}...${HEAD_BRANCH}" --pretty=format:"* [\`%h\`](http://github.com/seleniumhq/docker-selenium/commit/%H) - %s :: %an" --reverse >> release_notes.md
 
-CHROME_VERSION=$(docker run --rm selenium/node-chrome:${TAG_VERSION} google-chrome --version | awk '{print $5}')
+CHROME_VERSION=$(docker run --rm selenium/node-chrome:${TAG_VERSION} google-chrome --version | awk '{print $3}')
 EDGE_VERSION=$(docker run --rm selenium/node-edge:${TAG_VERSION} microsoft-edge --version | awk '{print $3}')
 CHROMEDRIVER_VERSION=$(docker run --rm selenium/node-chrome:${TAG_VERSION} chromedriver --version | awk '{print $2}')
 EDGEDRIVER_VERSION=$(docker run --rm selenium/node-edge:${TAG_VERSION} msedgedriver --version | awk '{print $4}')
