c4e7e1010c282e02e5aa1c412386edd325077a67
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2023-12-06 21:12:50 +0530

    Update to Selenium 4.16.0 [deploy]
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -7,7 +7,7 @@ if [ "${CI:-false}" = "false" ]; then
   source docker-selenium-tests/bin/activate
 fi
 
-python -m pip install selenium==4.15.0 \
+python -m pip install selenium==4.16.0 \
                       docker===6.1.3 \
                       | grep -v 'Requirement already satisfied'
 
