8b40c195a794ca6a6725a18b2386298dc2af9fca
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2023-12-08 18:29:06 +0530

    Add script to generate chart CHANGELOG after released
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- update_tag_in_docs_and_files.sh
+++ update_tag_in_docs_and_files.sh
@@ -11,14 +11,14 @@ echo -e "\033[0;32m LATEST_TAG -> ${LATEST_TAG}\033[0m"
 echo -e "\033[0;32m NEXT_TAG -> ${NEXT_TAG}\033[0m"
 
 # If you want to test this locally and you are using macOS, do `brew install gnu-sed` and change `sed` for `gsed`.
-find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/${LATEST_TAG}/${NEXT_TAG}/g"
+find . \( -type d -name .git -prune \) -o -type f ! -name 'CHANGELOG.md' -print0 | xargs -0 sed -i "s/${LATEST_TAG}/${NEXT_TAG}/g"
 
 echo -e "\033[0;32m Updating date used in some docs and files...\033[0m"
 echo -e "\033[0;32m LATEST_DATE -> ${LATEST_DATE}\033[0m"
 echo -e "\033[0;32m NEXT_DATE -> ${NEXT_DATE}\033[0m"
 
 # If you want to test this locally and you are using macOS, do `brew install gnu-sed` and change `sed` for `gsed`.
-find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s/${LATEST_DATE}/${NEXT_DATE}/g"
+find . \( -type d -name .git -prune \) -o -type f ! -name 'CHANGELOG.md' -print0 | xargs -0 sed -i "s/${LATEST_DATE}/${NEXT_DATE}/g"
 
 # Bump chart version and appVersion if next tag is different
 if [ "$latest_chart_app_version" == $LATEST_TAG ] && [ "$latest_chart_app_version" != "$NEXT_TAG" ]; then
