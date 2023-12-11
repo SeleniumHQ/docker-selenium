a76504a342f64838350fa5356356b244b6da27a6
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2023-12-11 07:12:49 +0530

    feat: changing owner while creating container for download support
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- Base/entry_point.sh
+++ Base/entry_point.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+_log () {
+    if [[ "$*" == "ERROR:"* ]] || [[ "$*" == "WARNING:"* ]] || [[ "${CONTAINER_LOGS_QUIET}" == "" ]]; then
+        echo "$@"
+    fi
+}
 
 #==============================================
 # OpenShift or non-sudo environments support
@@ -7,8 +12,37 @@
 
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+    echo "${USER_NAME:-${SEL_USER}}:x:$(id -u):0:${USER_NAME:-${SEL_USER}} user:${HOME}:${SE_DOWNLOAD_DIR}:/var:/opt:/sbin/nologin" >> /etc/passwd
   fi
+fi
+
+MKDIR_EXTRA=${SE_DOWNLOAD_DIR}","${MKDIR_EXTRA}
+CHOWN_EXTRA=${MKDIR_EXTRA}","${CHOWN_EXTRA}
+
+if [ -n "${MKDIR_EXTRA}" ]; then
+    for extra_dir in $(echo "${MKDIR_EXTRA}" | tr ',' ' '); do
+        _log "Creating directory ${extra_dir} ${MKDIR_EXTRA_OPTS:+(mkdir options: ${MKDIR_EXTRA_OPTS})}"
+        # shellcheck disable=SC2086
+        sudo mkdir ${MKDIR_EXTRA_OPTS:-"-p"} "${extra_dir}"
+    done
+fi
+
+if [ -n "${CHOWN_EXTRA}" ]; then
+    for extra_dir in $(echo "${CHOWN_EXTRA}" | tr ',' ' '); do
+        _log "Changing ${extra_dir} ownership. ${extra_dir} is owned by ${SEL_USER} ${CHOWN_EXTRA_OPTS:+(chown options: ${CHOWN_EXTRA_OPTS})}"
+        # shellcheck disable=SC2086
+        sudo chown ${CHOWN_EXTRA_OPTS:-"-R"} "${SEL_UID}:${SEL_GID}" "${extra_dir}"
+        sudo -E fix-permissions "${extra_dir}"
+    done
+fi
+
+# Raise error if the user isn't able to write files to download dir
+if [ -n "${CHOWN_EXTRA}" ]; then
+    for extra_dir in $(echo "${CHOWN_EXTRA}" | tr ',' ' '); do
+        if [[ ! -w ${extra_dir} ]]; then
+            _log "ERROR: no write access to download dir ${SE_DOWNLOAD_DIR}. Please correct the permissions and restart."
+        fi
+    done
 fi
 
 /usr/bin/supervisord --configuration /etc/supervisord.conf &
