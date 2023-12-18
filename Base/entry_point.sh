3e22a51441d62efd99215ce4ed82ef6e07256c55
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2023-12-18 15:01:37 +0530

    revert: changing owner while creating container for download support (#2069)
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- Base/entry_point.sh
+++ Base/entry_point.sh
@@ -1,34 +1,14 @@
 #!/usr/bin/env bash
-_log () {
-    if [[ "$*" == "ERROR:"* ]] || [[ "$*" == "WARNING:"* ]] || [[ "${CONTAINER_LOGS_QUIET}" == "" ]]; then
-        echo "$@"
-    fi
-}
 
-# If the container started as the root user
-if [ "$(id -u)" == 0 ]; then
-    fix-permissions "${SE_DOWNLOAD_DIR}"
-elif [ "$(id -u)" == "$(id -u ${SEL_USER})" ] && [ "$(id -g)" == "$(id -g ${SEL_USER})" ]; then
-    # Trust SEL_USER is the desired non-root user to execute with sudo
-    sudo -E fix-permissions "${SE_DOWNLOAD_DIR}"
-else
-    # For non-root user to change ownership
-    # Relaxing permissions for OpenShift and other non-sudo environments
-    # (https://docs.openshift.com/container-platform/latest/openshift_images/create-images.html#use-uid_create-images)
-    if ! whoami &> /dev/null; then
-        _log "There is no entry in /etc/passwd for our UID=$(id -u). Attempting to fix..."
-        if [ -w /etc/passwd ]; then
-            _log "Renaming user to ${USER_NAME:-default} ($(id -u):$(id -g)"
-            # We cannot use "sed --in-place" since sed tries to create a temp file in
-            # /etc/ and we may not have write access. Apply sed on our own temp file:
-            sed --expression="s/^${SEL_USER}:/${USER_NAME:-default}:/" /etc/passwd > /tmp/passwd
-            echo "${USER_NAME:-default}:x:$(id -u):$(id -g):${USER_NAME:-default} user:${HOME}:/bin/bash" >> /tmp/passwd
-            cat /tmp/passwd > /etc/passwd
-            rm /tmp/passwd
-            _log "Added new ${USER_NAME:-default} user ($(id -u):$(id -g)). Fixed UID!"
-        fi
-    fi
-    fix-permissions "${SE_DOWNLOAD_DIR}"
+#==============================================
+# OpenShift or non-sudo environments support
+# https://docs.openshift.com/container-platform/3.11/creating_images/guidelines.html#openshift-specific-guidelines
+#==============================================
+
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
 fi
 
 /usr/bin/supervisord --configuration /etc/supervisord.conf &
