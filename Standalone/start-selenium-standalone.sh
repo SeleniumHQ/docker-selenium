fb260f3d571b69f9986bebad3d89483a42a1ae70
edhinard <edouard.hinard@laposte.net>
2024-05-05 15:03:12 +0200

    start pulseaudio in standalone mode (#2243)
    start pulseaudio as in non standalone mode

--- Standalone/start-selenium-standalone.sh
+++ Standalone/start-selenium-standalone.sh
@@ -2,6 +2,16 @@
 #
 # IMPORTANT: Change this file only in directory Standalone!
 
+# Start the pulseaudio server
+pulseaudio -D --exit-idle-time=-1
+
+# Load the virtual sink and set it as default
+pacmd load-module module-virtual-sink sink_name=v1
+pacmd set-default-sink v1
+
+# set the monitor of v1 sink to be the default source
+pacmd set-default-source v1.monitor
+
 if [ ! -z "$SE_SUB_PATH" ]; then
   echo "Using SE_SUB_PATH: ${SE_SUB_PATH}"
   SUB_PATH_CONFIG="--sub-path ${SE_SUB_PATH}"
