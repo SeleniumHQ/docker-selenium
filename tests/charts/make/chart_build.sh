badfd9b885e4a00159327e41b947e44980ecf385
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2024-07-21 21:01:10 +0000

    chart(build): Push chart to Docker Hub registry
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- tests/charts/make/chart_build.sh
+++ tests/charts/make/chart_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -x
 
+SET_VERSION=${SET_VERSION:-"true"}
 CHART_PATH=${CHART_PATH:-"charts/selenium-grid"}
 # Function to be executed on command failure
 on_failure() {
@@ -24,14 +26,21 @@ fi
 
 python3 -m pip install yamale==4.0.4 \
                       yamllint==1.33.0 \
-                      | grep -v 'Requirement already satisfied'
+                      | grep -v 'Requirement already satisfied' || true
 
 cd ..
 rm -rf ${CHART_PATH}/Chart.lock
 ct lint --all --config tests/charts/config/ct.yaml
 # Helm dependencies build and lint is done by `ct lint` command
 rm -rf ${CHART_PATH}/../*.tgz
-helm package ${CHART_PATH} --version ${VERSION} --destination ${CHART_PATH}/..
+
+if [ "${SET_VERSION}" = "true" ]; then
+  ADD_VERSION="--version ${VERSION}"
+else
+  ADD_VERSION=""
+fi
+
+helm package ${CHART_PATH} ${ADD_VERSION} --destination ${CHART_PATH}/..
 
 readlink -f ${CHART_PATH}/../*.tgz > /tmp/selenium_chart_version
 cat /tmp/selenium_chart_version
