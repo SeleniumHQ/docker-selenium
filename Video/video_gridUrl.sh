52137a0e9852ae1fc15b682265b9457b2ae6509f
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2024-08-20 05:48:26 +0700

    Update Video/video_gridUrl.sh
    Co-authored-by: codiumai-pr-agent-pro[bot] <151058649+codiumai-pr-agent-pro[bot]@users.noreply.github.com>
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- Video/video_gridUrl.sh
+++ Video/video_gridUrl.sh
@@ -9,18 +9,11 @@ if [ "${SE_SUB_PATH}" = "/" ]; then
   SE_SUB_PATH=""
 fi
 
-if [ -z "${SE_HUB_HOST:-$SE_ROUTER_HOST}" ] || [ -z "${SE_HUB_PORT:-$SE_ROUTER_PORT}" ]; then
-  grid_url=""
-else
+grid_url="${SE_NODE_GRID_URL}"
+if [ -n "${SE_HUB_HOST:-$SE_ROUTER_HOST}" ] && [ -n "${SE_HUB_PORT:-$SE_ROUTER_PORT}" ]; then
   grid_url=${SE_SERVER_PROTOCOL}://${BASIC_AUTH}${SE_HUB_HOST:-$SE_ROUTER_HOST}:${SE_HUB_PORT:-$SE_ROUTER_PORT}${SE_SUB_PATH}
-fi
-
-if [ -z "${grid_url}" ] && [ -n "${DISPLAY_CONTAINER_NAME}" ] && [ "${SE_VIDEO_RECORD_STANDALONE}" = "true" ]; then
+elif [ -n "${DISPLAY_CONTAINER_NAME}" ] && [ "${SE_VIDEO_RECORD_STANDALONE}" = "true" ]; then
   grid_url="${SE_SERVER_PROTOCOL}://${BASIC_AUTH}${DISPLAY_CONTAINER_NAME}:${SE_NODE_PORT:-4444}${SE_SUB_PATH}" # For standalone mode
-fi
-
-if [ -z "${grid_url}" ]; then
-  grid_url="${SE_NODE_GRID_URL}"
 fi
 
 if [[ ${grid_url} == */ ]]; then
