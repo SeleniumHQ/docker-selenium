9106ba146d243e4cf8f26751dbe167fa823ef19c
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2024-08-19 23:17:04 +0000

    chart(fix): Remove alias from sub-chart to prevent render issue in other CD tools
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- tests/charts/make/chart_release.sh
+++ tests/charts/make/chart_release.sh
@@ -12,6 +12,13 @@ on_failure() {
 # Trap ERR signal and call on_failure function
 trap 'on_failure' ERR
 
+latest_chart_version=$(find . \( -type d -name .git -prune \) -o -type f -wholename '*/selenium-grid/Chart.yaml' -print0 | xargs -0 cat | grep ^version | cut -d ':' -f 2 | tr -d '[:space:]')
+helm template oci://registry-1.docker.io/${NAMESPACE}/selenium-grid --version ${latest_chart_version}
+if [[ $? -eq 0 ]]; then
+    echo "Chart version $latest_chart_version is already available in the registry"
+    exit 0
+fi
+
 CHART_PACKAGE_PATH=$(cat /tmp/selenium_chart_version)
 if [ -z "${CHART_PACKAGE_PATH}" ] || [ ! -f "${CHART_PACKAGE_PATH}" ]; then
     echo "Chart package path is empty. Please trigger chart_build.sh before this script."
