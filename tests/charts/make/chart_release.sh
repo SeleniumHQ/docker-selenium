f48c7c25d0c587a1df97b594b019667580d53050
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2024-08-20 07:33:37 +0000

    docs: update image tag in reference docs
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

--- tests/charts/make/chart_release.sh
+++ tests/charts/make/chart_release.sh
@@ -3,6 +3,14 @@ set -x
 
 NAMESPACE=${NAMESPACE:-"selenium"}
 # Function to be executed on command failure
+
+latest_chart_version=$(find . \( -type d -name .git -prune \) -o -type f -wholename '*/selenium-grid/Chart.yaml' -print0 | xargs -0 cat | grep ^version | cut -d ':' -f 2 | tr -d '[:space:]')
+helm template oci://registry-1.docker.io/${NAMESPACE}/selenium-grid --version ${latest_chart_version}
+if [[ $? -eq 0 ]]; then
+    echo "Chart version $latest_chart_version is already available in the registry"
+    exit 0
+fi
+
 on_failure() {
     local exit_status=$?
     echo "There is step failed with exit status $exit_status"
@@ -11,13 +19,6 @@ on_failure() {
 
 # Trap ERR signal and call on_failure function
 trap 'on_failure' ERR
-
-latest_chart_version=$(find . \( -type d -name .git -prune \) -o -type f -wholename '*/selenium-grid/Chart.yaml' -print0 | xargs -0 cat | grep ^version | cut -d ':' -f 2 | tr -d '[:space:]')
-helm template oci://registry-1.docker.io/${NAMESPACE}/selenium-grid --version ${latest_chart_version}
-if [[ $? -eq 0 ]]; then
-    echo "Chart version $latest_chart_version is already available in the registry"
-    exit 0
-fi
 
 CHART_PACKAGE_PATH=$(cat /tmp/selenium_chart_version)
 if [ -z "${CHART_PACKAGE_PATH}" ] || [ ! -f "${CHART_PACKAGE_PATH}" ]; then
