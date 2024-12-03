a7a649ed026de58219eb39eec0010ef45e7722b9
Viet Nguyen Duc <nguyenducviet4496@gmail.com>
2024-12-04 01:33:08 +0700

    Add docker-compose file for reference [skip ci]
    Signed-off-by: Viet Nguyen Duc <nguyenducviet4496@gmail.com>

---
+++ Sessions/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS sessions_map(
+    session_ids varchar(256),
+    session_caps text,
+    session_uri varchar(256),
+    session_stereotype text,
+    session_start varchar(256)
+ );
